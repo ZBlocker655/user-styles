13f2aa356a6e54866d43a6c2cf581cbd46fa3bd2
Zach Blocker <zach.blocker@gmail.com>
2023-06-16 03:23:51 -0400

    fix

--- workflowy/stylable-tags.user.js
+++ workflowy/stylable-tags.user.js
@@ -4,50 +4,38 @@
 // @author         LukeMT
 // @homepageURL    https://github.com/Zblocker655/user-styles
 // @updateURL      https://raw.githubusercontent.com/zblocker655/user-styles/main/workflowy/stylable-tags.user.js
+// @require        https://code.jquery.com/jquery-3.7.0.min.js
 // @include        http*://*workflowy.com/*
 // @version        1.1
 // ==/UserScript==
 (function() {
   'use strict';
 
-  // Helper function to check if a string ends with a given suffix
-  function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-  }
+  String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+  };
 
-  // Helper function to remove custom classes from an element
-  function removeCustomClasses(element) {
-    var classes = element.className.split(' ');
+  var customClasses = function(index, old){
+    var classes = old.split(" ");
     var custom = [];
-    for (var i = 0; i < classes.length; i++) {
-      if (endsWith(classes[i], '-proj')) {
-        custom.push(classes[i]);
-      }
+    for( i = 0; i < classes.length; i++){
+      if(classes[i].endsWith("-proj")){ custom.push(classes[i]);}
     }
-    element.className = custom.join(' ');
-  }
+    return custom.join(" ");
+  };
 
   var StylableTagsCounter = 1;
-  setInterval(function() {
-    StylableTagsCounter++;
-    if (StylableTagsCounter >= 3) {
-      var projectElements = document.getElementsByClassName('project');
-      var pageContainerElements = document.getElementsByClassName('pageContainer');
-      for (var i = 0; i < projectElements.length; i++) {
-        removeCustomClasses(projectElements[i]);
-      }
-      for (var j = 0; j < pageContainerElements.length; j++) {
-        removeCustomClasses(pageContainerElements[j]);
-      }
+
+  setInterval(function(){
+    StylableTagsCounter ++;
+    if( StylableTagsCounter >= 3){
+      $('.project').removeClass(customClasses);
+      $('.pageContainer').removeClass(customClasses);
       StylableTagsCounter = 0;
     }
-    var contentTagTextElements = document.querySelectorAll('span > .contentTagText');
-    contentTagTextElements.forEach(function(element) {
-      var x = element.textContent.toLowerCase();
-      var projectElement = element.closest('.project');
-      if (projectElement) {
-        projectElement.className += ' ' + x + '-proj';
-      }
-    });
-  }, 10);
+    $('span > .contentTagText').map( function(){
+      var x = $(this).text().toLowerCase();
+      $(this).closest('.project').addClass(x+"-proj");}
+    );
+  },10);
 })();
